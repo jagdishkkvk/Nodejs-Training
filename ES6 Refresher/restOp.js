ab660d3e9923ea17e688c84a583117e567c45cb6
Neha Sharma <nehasharma.ns12@gmail.com>
2020-10-12 22:15:05 +0530

    File System and Creating http Server

--- ES6 Refresher/restOp.js
+++
@@ -1,6 +0,0 @@
-const toArray = (...args) => {
-  return args;
-};
-
-console.log(toArray("a", "b")); //returns ['a','b']
-console.log(toArray(1, 2, 3, 4)); //returns [1,2,3,4]
---
+++ http-server-fs/fsModule.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+
+fs.writeFileSync("file1.txt", "Welcome to Nodejs Training");
+
+console.log("Program Ended");
